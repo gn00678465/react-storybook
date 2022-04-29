d7e09a9902a4aa6b1d6d69ba722e237fb7036ac7
Madao <gn00678465@gmail.com>
2022-04-29 22:05:05 +0800

    fix: modify storage

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -1,44 +1,48 @@
-class Storage {
-  constructor(getStorage = () => window.localStorage) {
-    this.storage = getStorage();
+export default function (namespace) {
+  if (!namespace) {
+    namespace = '';
   }
+  const namespacePrefix = namespace || '';
+  const legalNamespace = /^[a-zA-Z0-9_-]*$/;
+  if (!legalNamespace.test(namespace)) {
+    throw new TypeError('namespace 只接受字母和數字以及 _ 和 -');
+  }
+  return {
+    namespacePrefix: namespacePrefix,
+    set(key, value) {
+      if (value === undefined) {
+        this.remove(key);
+        return;
+      }
+      window.localStorage.setItem(
+        this.namespacePrefix + key,
+        this._serialize(value),
+      );
+      return value;
+    },
+    get(key, optionalDefaultValue) {
+      const data = window.localStorage.getItem(this.namespacePrefix + key);
+      return this._deserialize(data, optionalDefaultValue);
+    },
+    remove(key) {
+      window.localStorage.removeItem(this.namespacePrefix + key);
+    },
 
-  set(key, value) {
-    if (value === undefined) {
-      this.remove(key);
-      return;
-    }
-    this.storage.setItem(key, this._serialize(value));
-    return value;
-  }
-
-  get(key, optionalDefaultValue) {
-    const data = this.storage.getItem(key);
-    return this._deserialize(data, optionalDefaultValue);
-  }
-
-  remove(key) {
-    this.storage.removeItem(key);
-  }
-
-  clearAll() {
-    this.storage.clear();
-  }
-
-  _serialize(obj) {
-    return JSON.stringify(obj);
-  }
-
-  _deserialize(strVal, defaultVal) {
-    if (!strVal) return defaultVal;
-    let val = '';
-    try {
-      val = JSON.parse(strVal);
-    } catch (e) {
-      val = strVal;
-    }
-    return val !== undefined ? val : defaultVal;
-  }
+    clearAll() {
+      window.localStorage.clear();
+    },
+    _serialize(obj) {
+      return JSON.stringify(obj);
+    },
+    _deserialize(strVal, defaultVal) {
+      if (!strVal) return defaultVal;
+      let val = '';
+      try {
+        val = JSON.parse(strVal);
+      } catch (e) {
+        val = strVal;
+      }
+      return val !== undefined ? val : defaultVal;
+    },
+  };
 }
-
-export default Storage;
